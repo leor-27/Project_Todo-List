8a63846e1a5d3bb47ea36e1566afe6e997e9f058
JericoGatpandan <jericogatpandan0905@gmail.com>
2025-06-29 17:15:53 +0800

    fix: print new task in the document

--- src/ui.js
+++ src/ui.js
@@ -1,20 +1,8 @@
 import { createTodo } from "./todo";
 import { compareAsc, constructFrom, format } from "date-fns";
-import { TaskDialog } from ".";
 
 export function createWindow() {
-  const container = document.createElement("div");
-  container.className = "container";
-
-  const inputTask = document.createElement("input");
-  inputTask.setAttribute("type", "text");
-
-  const inputDate = document.createElement("input");
-  inputDate.setAttribute("type", "datetime-local");
-
-  const addTaskBtn = document.createElement("button");
-  addTaskBtn.className = "addTask";
-  addTaskBtn.textContent = "Add task";
+  const container = document.querySelector("#app");
 
   // Show modal
   const openTaskDialog = document.querySelector("#openTaskDialog");
@@ -35,42 +23,35 @@ export function createWindow() {
   const priorityValue = document.querySelector("#priority-dropdown");
   const projectName = document.querySelector("#project-dropdown");
 
-  const confirmBtn = document.getElementById("#confirm");
+  const confirmBtn = document.getElementById("confirm");
   confirmBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const title = titleValue.value;
     const description = descriptionValue.value;
     const dueDate = dueDateValue.value;
     const priority = priorityValue.value;
-    const projectLabel = projectName.value;
+    // const projectLabel = projectName.value;
 
-    showTask();
-  });
-
-  container.appendChild(inputTask);
-  container.appendChild(inputDate);
-  container.appendChild(addTaskBtn);
-
-  addTaskBtn.addEventListener("click", () => {
-    const inputValue = inputTask.value;
-    const rawDate = inputDate.value;
-    const formattedDate = format(new Date(rawDate), "PPp");
+    const formattedDate = format(new Date(dueDate), "PPp");
 
     const div = document.createElement("div");
     div.className = "task-container";
     const task = document.createElement("p");
     task.className = "task";
 
-    const todo = createTodo(inputValue, "Jerico", formattedDate, "any", false);
+    const todo = createTodo(title, description, formattedDate, priority, false);
     // task.textContent = JSON.stringify(todo, null, 2);
     task.textContent = `${todo.title} ${todo.description} ${todo.dueDate} ${todo.priority} ${todo.completed}`;
 
     div.appendChild(task);
     container.appendChild(div);
 
-    inputTask.value = "";
-    inputDate.value = "";
+    titleValue.value = "";
+    descriptionValue.value = "";
+    dueDateValue.value = "";
+    priorityValue.value = priority;
+    TaskDialog.close();
   });
-
-  // document.body.appendChild(container);
 }
+
+function showProjectDialog() {}
